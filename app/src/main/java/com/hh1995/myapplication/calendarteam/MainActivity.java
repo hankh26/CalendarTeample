c3f32062df483c75d29ba5c9f477aa4ede86c6e7
han <dlek26@naver.com>
2020-09-20 13:49:29 +0900

    Initial commit

---
+++ app/src/main/java/com/hh1995/myapplication/calendarteam/MainActivity.java
@@ -0,0 +1,16 @@
+package com.hh1995.myapplication.calendarteam;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        snjndkfjnsdjkdfnkjfsdnsdnakjsdnfjkasdnkfj
+    }
+}
